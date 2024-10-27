8a76d33051f4d158a38e78d84a5ecb86e9f0ef26
FirdausyTama <atama.firdausy@gmail.com>
2024-10-27 15:42:47 +0700

    CRUD_RESTAPI

--- routes/todo.js
+++ routes/todo.js
@@ -21,4 +21,27 @@ router.post('/', (req, res)=> {
     res.status(201).json(newTodo);
 });
 
+router.delete('/:id', (req, res) => {
+    const todoIndex = todos.findIndex (t => t.id === parseInt(req.params.id));
+    if (todoIndex === -1) return res.status(404).json({ message: 'Tugas tidak ditemukan'});
+    
+    const deletedTodo = todos.splice(todoIndex, 1)[0];
+    res.status(200).json({message: `Tugas'${deletedTodo.task}'telah dihapus`});
+
+});
+
+router.put('/:id', (req, res) => {
+    const todo = todos.find(t => t.id === parseInt(req.params.id));
+    if (!todo) return res.status(404).json ({message: 'Tugas tidak ditemukan'});
+    todo.task = req.body.task || todo.task;
+
+    res.status(200).json({
+        message: `Tugas dengan ID ${todo.id} telah diperbaharui`,
+        updateTodo: todo
+    });
+
+
+
+});
+
 module.exports = router;
